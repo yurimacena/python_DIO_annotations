172c2097fa5da665e58791d2cafc4f2eeb572465
yurimacena <yurimacenadev@gmail.com>
2024-07-01 15:40:02 -0300

    working with objects datetime, date and time

---
+++ data_hora_e_fuso_horario/1_datetime.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime, time
+
+data = date(2023, 7, 19)
+print(data)
+
+print(date.today())
+
+data_hora = datetime(2023, 7, 10, 10, 30, 20)
+print(data_hora)
+
+print(datetime.today())
+
+horas = time(10, 20, 0)
+print(horas)
